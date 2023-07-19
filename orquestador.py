e087f288a82badd5167617fc04aa9550d76e3f3f
Martín Ruiz Ptaskin <martinruizptaskin.7@gmail.com>
2023-07-19 19:59:13 -0300

    cambios

--- orquestador.py
+++ orquestador.py
@@ -7,21 +7,21 @@ This is a temporary script file.
 import time
 import os
 
-
+vueltas=0
 while 1==1:
 
- vueltas=0
+ 
  tiempo=0
- os.system('python scrapy/insiderstrack.py')
- os.system('python scrapy/maybeEtoroWin.py')
- os.system('python scrapy/superinvetors.py')
- while tiempo <1:
+ #os.system('python scrapy/insiderstrack.py')
+ #os.system('python scrapy/maybeEtoroWin.py')
+ #os.system('python scrapy/superinvetors.py')
+ while tiempo< 1:
      print(tiempo)
      tiempo=tiempo+1
      
      os.system('python scrapy/PriceStock.py')
   
-     time.sleep(800)
- vueltas= vueltas+1
+     time.sleep(60)
+ vueltas=+ 1
  print("vueltas ------------------- " + str(vueltas)+ "------------------------")
 
